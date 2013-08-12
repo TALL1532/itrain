9af1427a976565ea7c83cf5e306dd0d156dd7998
Thomas Deegan <tadeegan@gmail.com>
2013-08-11 21:14:09 -0500

    Added instructions into the control group task

---
+++ Classes/ModalInstructionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalInstructionsViewController.h
+//  Memory Training
+//
+//  Created by Thomas Deegan on 8/11/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalInstructionsViewController : UIViewController
+
+@end
---
+++ Classes/ModalInstructionsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ModalInstructionsViewController.m
+//  Memory Training
+//
+//  Created by Thomas Deegan on 8/11/13.
+//
+//
+
+#import "ModalInstructionsViewController.h"
+
+@interface ModalInstructionsViewController ()
+
+@end
+
+@implementation ModalInstructionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
